4903151415d28322bbc75273a5fc1a5c46b10721
franklynobleC <franklyessien01@gmail.com>
2023-01-20 20:41:03 -0500

    add mongoose model for jwt

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -1,25 +1,21 @@
 const User = require('../models/UserModel')
 const { StatusCodes } = require('http-status-codes')
 const {BadRequestError} = require('../error')
-const bcrypt = require('bcryptjs');
+const jwt = require ('jsonwebtoken')
+
 
 
 const register = async (req, resp) => {
- const {name, email, password} = req.body;
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt)
-
-  const tempUser = {name,email,password: hashedPassword}
-// 
-//
-     // console.log('post Method called')
-    
-     const user = await User.create({...tempUser}) // get  all data from the request  body
+    const user = await User.create({... req.body}) // get  all data from the request  body
+    const token = user.createJWT()
      
-    resp.status(StatusCodes.CREATED).json({user})
+    resp.status(StatusCodes.CREATED).json({user: {name: user.name }, token })
 }
-const login = async (req, resp) => {
+
+    // const token = jwt.sign({userId:user._id, name: user.name},'jwtSecret', {expiresIn: '30d'
+
+
+    const login = async (req, resp) => {
     resp.send(' login user')
 }
 
